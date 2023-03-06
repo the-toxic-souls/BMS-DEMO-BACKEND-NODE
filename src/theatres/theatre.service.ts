3fe30ddbd5724ac9c003ce3d1b870fe481a0aeab
nazim_gitlab <abdul.nazim@codeclouds.com>
2023-03-06 17:16:48 +0530

    added and make changes in theatre module

--- src/theatres/theatre.service.ts
+++ src/theatres/theatre.service.ts
@@ -1,22 +1,14 @@
 import { TheatreDTO } from "@/theatres/dtos/theatre.dto";
 import TheatreModel from "@/theatres/entities/theatre.entity";
 import { ObjectId } from "mongoose";
+import { TheatreBase } from "@/theatres/interfaces/theatre.interface";
 
 class TheatreService {
-    public list = async () => {
+    public list = async (): Promise<TheatreBase[]> => {
         return await TheatreModel.find();
     }
     public create = async (theatreDTO: TheatreDTO): Promise<ObjectId> => {
-        const newData = await TheatreModel.create({
-            name: theatreDTO.name,
-            address: theatreDTO.address,
-            phone: theatreDTO.phone,
-            seat: theatreDTO.seat,
-            seat_layouts: theatreDTO.seat_layouts,
-            types: theatreDTO.types,
-            category: theatreDTO.category,
-            description: theatreDTO.description
-        });
+        const newData = await TheatreModel.create(theatreDTO);
         const theatre = await newData.save();
         return theatre._id;
     }
